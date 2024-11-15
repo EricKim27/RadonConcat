ffcd1e4c112ca78de2a3634e91523228d1c5ebfa
EricKim27 <seohyun.kim@outlook.kr>
2024-11-15 11:46:45 +0900

    Modified Code to correctly calculate the effective dose

--- parse.py
+++ parse.py
@@ -2,4 +2,4 @@ def parsetext(text) -> list[str]:
     return text.split('	')
 
 def getefdose(text, time) -> float:
-    return text * 0.4 * 8 * (10 ** -6) * time
+    return text * 0.4 * 20.4 * (10 ** -6) * time
